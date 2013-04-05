3188860c2e2abcc74e81c9822fe788955467a739
Ben Rogers <rogers@farmerrogers.com>
2013-04-04 20:52:43 -0700

    GA lesson 12 updating users controller, and twitter views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,35 @@
 class UsersController < ApplicationController
   def index
+  	@users = User.order(:username).all
   end
 
   def show
+  	begin
+  		@user = User.find params[:id]
+  	rescue
+  		redirect_to users_path :alert => "No user by that id"
+  	end
   end
 
   def edit
+  	begin
+  		@user = User.find params[:id]
+  	rescue
+  		redirect_to users_path
+  	end
   end
 
   def update
+	begin  
+  		@user = User.find_by_id(params[:id])
+  		if @user.update_attributes(params[:user])
+  			redirect_to @user, notice: "User was saved successfully!"
+  		else
+  			render :edit
+  		end
+  	rescue
+  		render :edit
+  	end
   end
 
   def new
@@ -20,6 +41,14 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(params[:user])
-    @user.save!
+    if @user.valid? then
+    	if @user.save!
+    		redirect_to @user, notice: "User was saved successfully"
+    	else
+    		render :new
+    	end	
+    else
+    	render :new	
+    end
   end
 end
